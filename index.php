c242f1ff0c82c41e2f40a791703672fd217be5c5
Morita <branu.m.morita@gmail.com>
2016-07-15 16:11:18 +0900

    テスト確認

--- index.php
+++ index.php
@@ -1,48 +1,65 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-
+require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
-
+date_default_timezone_set('Asia/Tokyo');
 $app = new Silex\Application();
-
 $app->post('/callback', function (Request $request) use ($app) {
-    $client = new GuzzleHttp\Client();
-
     $body = json_decode($request->getContent(), true);
     foreach ($body['result'] as $msg) {
-        if (!preg_match('/(ぬるぽ|ヌルポ|ﾇﾙﾎﾟ|nullpo)/i', $msg['content']['text'])) {
-            continue;
-        }
-
-        $resContent = $msg['content'];
-        $resContent['text'] = 'ｶﾞｯ';
-
-        $requestOptions = [
-            'body' => json_encode([
-                'to' => [$msg['content']['from']],
-                'toChannel' => 1383378250, # Fixed value
-                'eventType' => '138311608800106203', # Fixed value
-                'content' => $resContent,
-            ]),
-            'headers' => [
-                'Content-Type' => 'application/json; charset=UTF-8',
-                'X-Line-ChannelID' => getenv('1474063906'),
-                'X-Line-ChannelSecret' => getenv('5adc405305f82659ee546429cc86cb60'),
-                'X-Line-Trusted-User-With-ACL' => getenv('uad8d0a29ea46f52741f54eb530f3628f'),
+        //fromとメッセージを取得
+        $from = $msg['content']['from'];
+        $message = $msg['content']['text'];
+        //Redisからcontextを取得
+        $redis = new Predis\Client(getenv('REDIS_URL'));
+        $context = $redis->get($from);
+        //雑談対話APIを叩く
+        $response = dialogue($message, $context);
+        //contextをRedisに保存する
+        $redis->set($from, $response->context);
+        //LINEに返信
+        $post_data = [
+            "to" => [
+                $from
             ],
-            'proxy' => [
-                'https' => getenv('FIXIE_URL'),
-            ],
+            "toChannel" => "1383378250",
+            "eventType" => "138311608800106203",
+            "content" => [
+                "contentType" => 1,
+                "toType" => 1,
+                "text" => $response->utt
+            ]
         ];
-
-        try {
-            $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
-        } catch (Exception $e) {
-            error_log($e->getMessage());
-        }
+        $ch = curl_init("https://trialbot-api.line.me/v1/events");
+        curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+        curl_setopt($ch, CURLOPT_PROXY, getenv('FIXIE_URL'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Content-Type: application/json; charser=UTF-8",
+            "X-Line-ChannelID: ". getenv('1474063906'),
+            "X-Line-ChannelSecret: ". getenv('5adc405305f82659ee546429cc86cb60'),
+            "X-Line-Trusted-User-With-ACL: ". getenv('uad8d0a29ea46f52741f54eb530f3628f')
+        ]);
+        curl_exec($ch);
+        curl_close($ch);
     }
-
-    return 'OK';
+    return 0;
 });
 
+function dialogue($message, $context) {
+    $post_data = array('utt' => $message);
+    $post_data['context'] = $context;
+    // DOCOMOに送信
+    $ch = curl_init("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=". getenv('422f344f62564d424e3454696a74425867505a33423231726b664f346532555467352e4673536344456142'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Content-Type: application/json; charser=UTF-8"
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($result);
+}
+
 $app->run();
+
